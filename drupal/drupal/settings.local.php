9f2879c7d0da92b23426ea568956ab593f611d0e
Jian He <hejian.he@gmail.com>
2017-11-25 00:43:16 +0800

    fix

---
+++ drupal/drupal/settings.local.php
@@ -0,0 +1,5 @@
+<?php
+$settings['cache']['default'] = 'cache.backend.memcache_storage';
+$settings['memcache_storage']['key_prefix'] = '';
+$settings['memcache_storage']['memcached_servers'] = ['127.0.0.1:11211' => 'default'];
+$settings['memcache_storage']['debug'] = TRUE;
