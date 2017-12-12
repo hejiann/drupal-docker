b73f8b47dcba37baa1da796a438261b51a7ff04b
Jian He <hej@eabax.com>
2017-12-12 05:01:21 -0500

    fix

--- drupal/build.sh
+++ drupal/build.sh
@@ -1,19 +1,5 @@
-#!/bin/bash
+#!/bin/sh
 
-if ! type "docker" > /dev/null; then
-  wget -qO- https://get.docker.com/ | sh
-  #curl -sSL http://acs-public-mirror.oss-cn-hangzhou.aliyuncs.com/docker-engine/internet | sh -
-fi
-
-if ! type "docker-compose" > /dev/null; then
-  sudo apt-get install -y python-pip
-  sudo pip install docker-compose
-fi
-
-sudo usermod -aG docker $USER
-sudo usermod -aG www-data $USER
-
-newgrp docker <<EONG
 docker pull jianhe/drupal
 sed -i 's/volumes:/#volumes:/' docker-compose.yml
 sed -i 's/- .\/web:\/var\/www\/html/#- .\/web:\/var\/www\/html/' docker-compose.yml
@@ -28,7 +14,6 @@ sudo docker cp drupal:/var/www/html ./web
 docker-compose stop
 sed -i 's/#volumes:/volumes:/' docker-compose.yml
 sed -i 's/#- .\/web:\/var\/www\/html/- .\/web:\/var\/www\/html/' docker-compose.yml
-EONG
 
 sudo chown www-data.www-data ./web -R
 sudo chmod g+w ./web -R
