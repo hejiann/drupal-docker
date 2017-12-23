32eca13be39cbfcef29cf978fc725c3813e67a36
Jian He <hejian.he@gmail.com>
2017-12-23 13:46:28 +0800

    fix

--- drupal/build.sh
+++ drupal/build.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
 docker pull jianhe/drupal
+
+docker rm -f drupal
 docker run -d --name drupal jianhe/drupal
 if [ -d ./web.old ]; then
   sudo rm ./web.old -rf
@@ -10,6 +12,7 @@ if [ -d ./web ]; then
 fi
 sudo docker cp drupal:/var/www/html ./web
 docker stop drupal
+docker rm drupal
 
 sudo chown www-data.www-data ./web -R
 sudo chmod g+w ./web -R
