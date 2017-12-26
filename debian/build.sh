272816dcb7c5190f50e0eff9436b3a271963e6e8
Jian He <hejian.he@gmail.com>
2017-12-26 21:07:14 +0800

    fix

--- debian/build.sh
+++ debian/build.sh
@@ -1,20 +1,19 @@
 #!/bin/bash
 
 docker pull jianhe/debian
-sed -i 's/volumes:/#volumes:/' docker-compose.yml
-sed -i 's/- .\/web:\/var\/www\/html/#- .\/web:\/var\/www\/html/' docker-compose.yml
-docker-compose up -d
+
+docker rm -f debian
+docker run -d --name debian jianhe/debian
 if [ -d ./web.old ]; then
-  rm ./web.old -rf
+  rm -rf ./web.old
 fi
 if [ -d ./web ]; then
   mv ./web ./web.old
 fi
 sudo docker cp debian:/var/www/html ./web
-docker-compose stop
-sed -i 's/#volumes:/volumes:/' docker-compose.yml
-sed -i 's/#- .\/web:\/var\/www\/html/- .\/web:\/var\/www\/html/' docker-compose.yml
+docker stop debian
+docker rm debian
 
-sudo chown www-data.www-data ./web -R
-sudo chmod g+w ./web -R
+sudo chown -R www-data.www-data ./web
+sudo chmod -R g+w ./web
 
--- drupal/build.sh
+++ drupal/build.sh
@@ -5,7 +5,7 @@ docker pull jianhe/drupal
 docker rm -f drupal
 docker run -d --name drupal jianhe/drupal
 if [ -d ./web.old ]; then
-  sudo rm ./web.old -rf
+  sudo rm -rf ./web.old
 fi
 if [ -d ./web ]; then
   mv ./web ./web.old
@@ -14,6 +14,6 @@ sudo docker cp drupal:/var/www/html ./web
 docker stop drupal
 docker rm drupal
 
-sudo chown www-data.www-data ./web -R
-sudo chmod g+w ./web -R
+sudo chown -R www-data.www-data ./web
+sudo chmod -R g+w ./web
 
