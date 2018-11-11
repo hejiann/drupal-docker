be1fd7bb0deeaf6725b552e0a08232c49f197349
Jian He <hejian.he@gmail.com>
2018-11-11 14:31:19 +0800

    fix

--- drupal/drupal/settings.local.php
+++ drupal/drupal/settings.local.php
@@ -1,8 +1,11 @@
 <?php
-$settings['cache']['default'] = 'cache.backend.memcache_storage';
-
-$settings['memcache']['key_prefix'] = '';
 $settings['memcache']['servers'] = ['127.0.0.1:11211' => 'default'];
+$settings['memcache']['bins'] = ['default' => 'default'];
+$settings['memcache']['key_prefix'] = '';
 
-$settings['memcache_storage']['key_prefix'] = '';
-$settings['memcache_storage']['memcached_servers'] = ['127.0.0.1:11211' => 'default'];
+$settings['cache']['default'] = 'cache.backend.memcache';
+
+#$settings['memcache_storage']['key_prefix'] = '';
+#$settings['memcache_storage']['memcached_servers'] = ['127.0.0.1:11211' => 'default'];
+
+#$settings['cache']['default'] = 'cache.backend.memcache_storage';
